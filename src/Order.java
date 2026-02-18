b426e818f12dfa1c8330ef88c9ce6587e3a655af
alina.marushchenko <alina.marushchenko@infis.cz>
2026-02-18 13:46:44 +0100

    client almost done

--- src/Order.java
+++ src/Order.java
@@ -4,13 +4,13 @@ import java.util.UUID;
 
 public final class Order implements Serializable {
     private static final long serialVersionUID = 1L;
-    public final int requestId;
+    public int requestId = 0;
     public final OrderType type;
     public final String itemName;
     public final int qty;
 
-    public Order(int requestId, OrderType type, String itemName, int qty) {
-        this.requestId = requestId;
+    public Order( OrderType type, String itemName, int qty) {
+        this.requestId++;
         this.type = type;
         this.itemName = itemName;
         this.qty = qty;
