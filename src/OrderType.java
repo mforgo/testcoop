875396c06388ee9515cb627cb32618e430ed22f7
alina.marushchenko <alina.marushchenko@infis.cz>
2026-02-18 13:03:59 +0100

    files added

---
+++ src/OrderType.java
@@ -0,0 +1,6 @@
+import java.io.Serializable;
+
+public enum OrderType implements Serializable {
+    PUT,
+    TAKE
+}
---
+++ src/StateRequest.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+public final class StateRequest implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    public final int requestId;
+
+    public StateRequest(int requestId) {
+        this.requestId = requestId;
+   }
+}
